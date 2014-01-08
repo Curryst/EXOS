46a4f29987752f196cddbc516288427c0dbaf87d
Seth Curry <scurry@scurry-lap.krypto.io>
2014-01-07 19:50:39 -0500

    Added some build up for initializing the GDT

--- boot.s
+++ boot.s
@@ -1,8 +1,8 @@
 .set ALIGN,		1<<0
-.set MEMINFO,	1<<1
+.set MEMINFO,		1<<1
 .set FLAGS,		ALIGN | MEMINFO
 .set MAGIC,		0x1BADB002
-.set CHECKSUM,	-(MAGIC + FLAGS)
+.set CHECKSUM,		-(MAGIC + FLAGS)
 
 .section .multiboot
 .align 4
--- kernel.c
+++ kernel.c
@@ -38,6 +38,34 @@ enum vga_color
 	COLOR_WHITE = 15,
 };
 
+void encodeGdtEntry(uint8_t *target, struct GDT source)
+{
+	// Check the limit to make sure it can be encoded
+	if ((source.limit > 65536> && (soure.limit & 0xFFF) != 0xFFF)) {
+		kerror("You can't do that");
+	}
+	if (source.limit > 65536) {
+		// Adjust granularity if required
+		source.limit = source.limit >> 12;
+		target[6] = 0xC0;
+	} else {
+		target[6] = 0x40;
+	}
+
+	// Encode the limit
+	target[0] = source.limit & 0xFF;
+	target[1] = (source.limit >> 8) & 0xFF;
+	target[6] |= (source.limit >> 16) & 0xF;
+
+	// Encode the base
+	target[2] = source.base & 0xFF;
+	target[3] = (source.base >> 8) &0xFF;
+	target[4] = (source.base >> 16) & 0xFF;
+	target[7] = (source.base >> 24) & 0xFF;
+
+	target[5] = source.type;
+}
+
 uint8_t make_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
